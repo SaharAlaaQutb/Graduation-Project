5b2e5a9722f4138586d07250360abb8e855a482d
Sahar Alaa <38354077+SaharAlaaQutb@users.noreply.github.com>
2020-04-19 15:33:58 +0200

    Update main-project-code.py

--- main-project-code.py
+++ main-project-code.py
@@ -5,7 +5,18 @@ import sys
 designFile = loadUi('project(main).ui')
 
 class window(QtWidgets.QMainWindow , designFile):
-    pass
-    
+    def __init__(self, parent = none):
+        super(window, self).__init__(parent)
+        QMainWindow.__init__(self)
+        self.setupUi(self)
 
+
+def main():
+    app = QApplication(sys.argv)
+    login = window()
+    login.show()
+    app.exec_()
+
+if __name__ == '__main__':
+    main()
 
